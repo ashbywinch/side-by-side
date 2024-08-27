566c050c1673f1743d673f954192839902526a2f
Ashby Winch <github@blueumbrella.net>
2024-08-27 17:08:29 +0100

    Removing routing dependencies from subcomponents (#25)

--- components.d.ts
+++ components.d.ts
@@ -7,22 +7,12 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    BAlert: typeof import('bootstrap-vue-next')['BAlert']
-    BButton: typeof import('bootstrap-vue-next')['BButton']
-    BCard: typeof import('bootstrap-vue-next')['BCard']
-    BCardBody: typeof import('bootstrap-vue-next')['BCardBody']
-    BCardText: typeof import('bootstrap-vue-next')['BCardText']
     BookView: typeof import('./src/components/BookView.vue')['default']
     BPagination: typeof import('bootstrap-vue-next')['BPagination']
-    copy: typeof import('./src/components/Index copy.vue')['default']
-    Index: typeof import('./src/components/Index.vue')['default']
     IndexDetailsView: typeof import('./src/components/IndexDetailsView.vue')['default']
     IndexFilter: typeof import('./src/components/IndexFilter.vue')['default']
-    IndexNav: typeof import('./src/components/IndexNav.vue')['default']
-    IndexPage: typeof import('./src/components/IndexPage.vue')['default']
     IndexPagination: typeof import('./src/components/IndexPagination.vue')['default']
     IndexView: typeof import('./src/components/IndexView.vue')['default']
-    IndexWrapper: typeof import('./src/components/IndexWrapper.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
